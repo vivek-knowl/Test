d8ec25b4ab1eeca76e3207d09c0a617464438e42
vivek-knowl <vivek@knowl.io>
2023-07-27 13:01:22 +0530

    test

--- main.py
+++ main.py
@@ -9,16 +9,42 @@ def multiply(x, y):
 
 def divide(x, y):
     if y == 0:
-        return "Error: Cannot divide by zero"
+        return "Cannot divide by zero"
     return x / y
 
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+def calculator():
+    print("Welcome to the Simple Calculator!")
+    print("Operations:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
 
-while True:
-    choice = input("Enter choice (1/2/3/4): ")
+    while True:
+        choice = input("Enter operation number (1/2/3/4/5): ")
 
-    if choice in ('1', '2', '3', '4'):
+        if choice == '5':
+            print("Goodbye!")
+            break
+
+        if choice not in ['1', '2', '3', '4']:
+            print("Invalid choice. Please try again.")
+            continue
+
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print("Result: ", add(num1, num2))
+        elif choice == '2':
+            print("Result: ", subtract(num1, num2))
+        elif choice == '3':
+            print("Result: ", multiply(num1, num2))
+        elif choice == '4':
+            print("Result: ", divide(num1, num2))
+        else:
+            print("Invalid input. Please try again.")
+
+if __name__ == "__main__":
+    calculator()
